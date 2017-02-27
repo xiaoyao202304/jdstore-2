e73d36580974555bcc41d571ab37fb84864423a4
yaoyao <yao_zhou@icloud.com>
2017-02-26 23:59:56 -0800

    add notify_cancel/ship function

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
         post :cancel
         post :ship
         post :shipped
-        delete :return
+        post :return
       end
     end
   end
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
